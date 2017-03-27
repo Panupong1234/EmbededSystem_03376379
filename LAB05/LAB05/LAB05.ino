afdd0be617e93593909970a6eb0688ff3beb2aa0
Panupong <56030319@kmitl.ac.th>
2017-03-27 12:55:09 +0700

    LAB05

---
+++ LAB05/LAB05/LAB05.ino
@@ -0,0 +1,17 @@
+void setup() {
+  // initialize serial communication at 9600 bits per second:
+  Serial.begin(9600);
+  pinMode(A5,OUTPUT);
+}
+// the loop routine runs over and over again forever:
+void loop() {
+  // read the input on analog pin 0:
+  int sensorValue = analogRead(A0);
+  if(sensorValue > 150) 
+      digitalWrite(A5,LOW);
+  else
+      digitalWrite(A5,HIGH);
+  // print out the value you read:
+  Serial.println(sensorValue);
+  delay(1);        // delay in between reads for stability
+}
